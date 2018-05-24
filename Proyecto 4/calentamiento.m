8e95f2a30c417818efd36c1c764bbc26e9c6dcb5
Daniel Alvarado <dab18@hotmail.com>
2018-05-24 14:04:09 -0600

    First commit
    First

---
+++ Proyecto 4/calentamiento.m
@@ -0,0 +1,12 @@
+function A = warmUpExercise()
+%hay que retornar una matriz 5x5
+
+A = [];
+% ============= SU CÓDIGO ==============
+
+A = [1,2,3,4,5;6,7,8,9,10]
+
+% ===========================================
+
+
+end
