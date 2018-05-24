63824b251b24237f48ca58df7a3d7ac7235932c2
Daniel Alvarado <dab18@hotmail.com>
2018-05-24 14:20:23 -0600

    graficar.m
    Esta justo como en el doc del profe pero no corre.

--- Proyecto 4/graficar.m
+++ Proyecto 4/graficar.m
@@ -1,7 +1,8 @@
 function graficar(x, y)
 %Grafica.
-
-
+plot(x,y,'rx','MarketSize',10);
+ylabel('Ganancia en $10,000s');
+xlabel('[Poblacion de la ciudad en 10,000s');
 figure; % abrir una ventana
 
 
